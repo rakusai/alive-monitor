c2b6d813867e7561e2f344163e9ed82489ab4b5f
Isshu Rakusai <rakusai@gmail.com>
2011-12-02 19:17:08 +0900

    Small update

--- application.py
+++ application.py
@@ -119,17 +119,20 @@ class EditPage(webapp.RequestHandler):
         
         self.redirect("/")
         
-def email_notification(entry):
+def email_notification(entry,additional_message = ""):
     subject = ''
     body = ''
     if entry.alive:
         subject = entry.title + " is alive"
     else:
         subject = entry.title + " is down (" + entry.error_reason + ")"
+    if additional_message:
+        subject += " (" + additional_message + ")"
+        
     body = subject + "\n\n" + "http://alive-monitor.appspot.com/"
         
     mail.send_mail(sender="Alive Monitor <rakusai@gmail.com>",
-                  to="rakusai@gmail.com",
+                  to="server@nota.jp",
                   subject=subject,
                   body=body)        
     logging.info("Sent Email:" + subject)
@@ -143,7 +146,7 @@ class Check(webapp.RequestHandler):
             alive = True
             error_reason = ''
             try:
-                result = urlfetch.fetch(entry.url,deadline=10)
+                result = urlfetch.fetch(entry.url,deadline=30)
                 if result.status_code != 200:
                     alive = False
                     error_reason = str(result.status_code) + " Error"
@@ -162,16 +165,19 @@ class Check(webapp.RequestHandler):
                 error_reason = "Enexpected Error"
             
             if entry.alive != alive or not entry.started:
+                admsg = ""
+                if not entry.started:
+                    admsg = "Monitor Start"
                 entry.started = True
                 entry.error_reason = error_reason
                 entry.alive = alive
-                email_notification(entry)
+                email_notification(entry,admsg)
                 entry.put()
             else:
                 if not alive:
                     diff = datetime.datetime.now() - entry.update
                     if diff.seconds > 60*60:
-                        email_notification(entry)
+                        email_notification(entry,"More than 1 hour!")
                         entry.put()                
 
         now = datetime.datetime.now()
